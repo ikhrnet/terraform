d1c73362ec5d0b9851079bd32c60e2a91b458b18
Tatsuya Ikehara <ikhrnet@users.noreply.github.com>
2019-12-12 08:56:30 +0900

    Add variables written in terraform.tfvars to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "aws_access_key" {
+  type        = string
+  description = "The AWS access key id associated with the profile"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "The AWS secret access key associated with the profile"
+}
+
+variable "organization" {
+  type        = string
+  description = "The target organization name in Terraform Cloud"
+}
+
 variable "profile" {
   type        = string
   description = "The target profile of AWS credentials"
@@ -6,5 +21,10 @@ variable "profile" {
 variable "region" {
   type        = string
   default     = "us-east-1"
-  description = "The default region of AWS to use"
+  description = "The default region of AWS"
+}
+
+variable "workspace" {
+  type        = string
+  description = "The target workspace name in Terraform Cloud"
 }
