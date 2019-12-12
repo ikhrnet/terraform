6d105c3a92e2b1e83f34b24571b1e8f69fc5bb3a
Tatsuya Ikehara <ikhrnet@users.noreply.github.com>
2019-12-13 05:44:47 +0900

    Add VPC default configurations

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,13 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block                       = "10.0.0.0/16"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = false
+  enable_classiclink               = false
+  enable_classiclink_dns_support   = false
+  assign_generated_ipv6_cidr_block = false
+
+  tags = {
+    Name = "Terraform"
+  }
 }
